04a90ab68e7eb950fb40cdf5dd40c786feb4b9f8
doram9 <dodo0207k@naver.com>
2022-08-11 21:48:50 +0900

    DELETE HTTP method 구현

--- my-restful-services/src/main/java/com/example/myrestfulservices/user/UserDaoService.java
+++ my-restful-services/src/main/java/com/example/myrestfulservices/user/UserDaoService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -37,6 +38,20 @@ public class UserDaoService {
                 return user;
             }
         }
+        return null;
+    }
+
+    public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator(); //iterator 형태로 변환
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+
+            if (user.getId() == id) {
+                iterator.remove(); //iterator 변수에 접근해야 users데이터가 변경됌.
+                return user;
+            }
+        }
+
         return null;
     }
 }
